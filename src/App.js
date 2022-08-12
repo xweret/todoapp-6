393857a8215d3cef39473e4a03d755b91c71fc92
Gabriel Garofalo <gabriel695681@gmail.com>
2022-08-11 21:28:49 -0300

    add item

--- src/App.js
+++ src/App.js
@@ -1,11 +1,60 @@
+import React, { Component } from 'react'
 
+export class App extends Component {
 
-function App() {
-  return (
-    <div className="App">
-    
-    </div>
-  );
+// creo el constructor 
+
+constructor(props){
+  super(props);
+  this.state = {
+    newItem: "",
+    list: []
+  }
 }
 
-export default App;
+updateInput(key, value){
+  this.setState({
+    [key]: value
+  })
+}
+
+addItem(){
+  const newItem = {
+    id: 1 + Math.random(),
+    value: this.state.newItem.slice()
+  };
+
+  const list = [...this.state.list]
+  list.push(newItem)
+  this.setState({
+    list,
+    newItem:""
+  })
+
+  }
+
+
+
+  render() {
+    return (
+      <div>
+
+      Add an item to the list...
+
+      <br/>
+      <input
+      type='text'
+      placeholder='agregar a la lista'
+      value={this.state.newItem}
+      onChange= {e =>this.updateInput('newItem', e.target.value)}
+      />
+      <button
+      onClick={()=> this.addItem()}
+      > Agregar </button>
+      
+      </div>
+    )
+  }
+}
+
+export default App
